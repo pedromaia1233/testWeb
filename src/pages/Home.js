3818e79f2bf269fea700009b0fda8b2ebfde0afa
Pedro Maia <p.maia@campus.fct.unl.pt>
2021-12-22 18:26:02 +0000

    change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,18 @@ import BannerImage1 from "../assets/introImage1.jpg";
 import BannerImage2 from "../assets/home2.jpg";
 import BannerImage3 from "../assets/home3.jpg";
 import "../styles/Home.css";
-import { useEffect, useState } from "react"
+import { useEffect, useState, useLayoutEffect  } from "react"
 
 function Home() {
   const [counter, setCounter] = useState(1);
-  const [selected, setSelected] = useState(BannerImage1);
+  const [selected, setSelected] = useState("https://images-pw.pixieset.com/elementfield/69219295/4D7A8890-08d25b2d.jpg");
   const [title, setTitle] = useState("FOTOGRAFIA É POESIA");
   const [subTitle, setSubTitle] = useState("A BELEZA DO MOMENTO IMPORTA");
 
   const changeBack = (counterAux) =>{ 
     switch (counterAux) {
       case 1:
-        setSelected(BannerImage2)
+        setSelected("https://images-pw.pixieset.com/elementfield/30319295/4D7A0074PB-876f2e22.jpg")
         setTitle("FOTOGRAFIA É AMOR")
         setSubTitle("PARTILHA-A COM QUEM MAIS AMAS")
         setCounter(2)
@@ -26,7 +26,7 @@ function Home() {
         setSubTitle("CRIA UMA IMAGEM, ETERNIZA O MOMENTO")
         break;
       case 3:
-        setSelected(BannerImage1)
+        setSelected("https://images-pw.pixieset.com/elementfield/69219295/4D7A8890-08d25b2d.jpg")
         setCounter(1)
         setTitle("FOTOGRAFIA É POESIA")
         setSubTitle("A BELEZA DO MOMENTO IMPORTA")
@@ -36,7 +36,7 @@ function Home() {
     }
   }
 
-  useEffect(()=> {
+  useLayoutEffect(()=> {
     setTimeout(() => {changeBack(counter)} , 4000);
   },[counter])
 
